c5c1339c24ea2977e376412894ce4f7744f5e14b
mrbrainz <mrbrainz@users.noreply.github.com>
2023-08-12 21:46:55 +0100

    Wrapping some functions and beginning logic to handle string replacements

--- index.php
+++ index.php
@@ -2,56 +2,97 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
-$fontDirs = $defaultConfig['fontDir'];
 
-$defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
-$fontData = $defaultFontConfig['fontdata'];
 
 $templatehtml = file_get_contents('art.html');
 
-$stamp = time();
-$tempdir = "temp";
-$prefix = "sub-art-";
-$outputfile = $tempdir."/".$prefix.$stamp;
-$stylesheet = file_get_contents('normalize.css');
-$stylesheet .= file_get_contents('style.css');
 
-$mpdf = new \Mpdf\Mpdf([
-    'mode' => 'utf-8', 
-    'format' => [282.3, 282.3],
-    'fontDir' => array_merge($fontDirs, [
-        __DIR__ . '/fonts',
-    ]),
-    'fontdata' => $fontData +[ 
-        'pragmatica' => [
-            'R' => 'PragmaticaMedium.ttf'
-        ],
-        'nimbus-sans-ultralight' => [
-            'R' => 'NimbusSansNovusT-UltraLight.ttf'
+
+function sanitiseFilename($file) {
+    // Remove anything which isn't a word, whitespace, number
+    // or any of the following caracters -_~,;[]().
+    $file = mb_ereg_replace("([^\w\s\d\-_~,;\[\]\(\).])", '', $file);
+    // Remove any runs of periods
+    $file = mb_ereg_replace("([\.]{2,})", '', $file);
+    
+    return $file;
+}
+
+ function is_base64($s) {
+      return (bool) preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s);
+}
+
+function createStylesheet($id,$imagefile) {
+    $stylesheet = file_get_contents('css/normalize.css');
+    $stylesheet .= file_get_contents('css/template-'.intval($id).'.css');
+    $imagefile = sanitiseFilename($imagefile);
+    if (file_exists("djs/".$imagefile)) {
+        $imagefile = "djs/".$imagefile;
+    } else {
+        $imagefile = "img/t".intval($id)."-default.jpg";
+    }
+        
+    $stylesheet = str_replace("{{image}}", $imagefile, $stylesheet);
+        
+    return $stylesheet;
+}
+
+$brainzstyle = createStylesheet(1,"brainz.jpg");
+
+function createPDFBlob($stylesheet,$html) {
+    
+    // Add config for custom font folder
+    $defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+    $fontDirs = $defaultConfig['fontDir'];
+
+    $defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+    $fontData = $defaultFontConfig['fontdata'];
+
+    $mpdf = new \Mpdf\Mpdf([
+        'mode' => 'utf-8', 
+        'format' => [282.3, 282.3],
+        'fontDir' => array_merge($fontDirs, [
+            __DIR__ . '/fonts',
+        ]),
+        'fontdata' => $fontData +[ 
+            'pragmatica' => [
+                'R' => 'PragmaticaMedium.ttf'
+            ],
+            'nimbus-sans-ultralight' => [
+                'R' => 'NimbusSansNovusT-UltraLight.ttf'
+            ]
+            ,
+            'nimbus-sans-regular' => [
+                'R' => 'NimbusSanL-Reg.ttf'
+            ]
         ]
-        ,
-        'nimbus-sans-regular' => [
-            'R' => 'NimbusSanL-Reg.ttf'
-        ]
-    ]
-    ]);
-$mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
-$mpdf->WriteHTML($templatehtml,\Mpdf\HTMLParserMode::HTML_BODY);
-/* $mpdf->Output($outputfile.".pdf", 'F'); */
+        ]);
+    $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
+    $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
+    
+    return $mpdf->Output('', 'S');
+}
 
-$pdfoutput = $mpdf->Output('', 'S');
+$pdfoutput = createPDFBlob($brainzstyle,file_get_contents('art.html'));
 
+function pdfToBase64($blob,$format) {
+    if ($format !== "jpg" && $format !== "png") {
+        $format = "jpg";
+    }
 
-$image = new Imagick();
-$image->readImageBlob($pdfoutput);
-$image->setResolution( 1080, 1080 );
-$image->setImageFormat( "jpg" );
-$image->setImageCompressionQuality(80);
-//$image->writeImage($outputfile.'.png');
-$imageBlob = "data:image/jpg;base64, ".base64_encode($image->getImageBlob());
-echo '<img src="'.$imageBlob.'" alt="" />';
-exit();
+    $image = new Imagick();
+    $image->readImageBlob($blob);
+    $image->setResolution( 1080, 1080 );
+    $image->setImageFormat( $format );
+    $image->setImageCompressionQuality(80);
+    $imageBase64 = "data:image/".$format.";base64, ".base64_encode($image->getImageBlob());
+    
+    return $imageBase64;
+    
+}
+
+$art = pdfToBase64($pdfoutput,"jpg");
+
 ?>
 <!doctype html>
 <html>
@@ -65,5 +106,8 @@ exit();
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <?php echo '<img src="'.$art.'" alt="" />'; ?>
 </body>
 </html>
+
+<?php exit();
