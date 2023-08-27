893afa984e0bfa7ef9d28d1c3e6214ab9d5d33ed
mrbrainz <mrbrainz@users.noreply.github.com>
2023-08-27 15:08:25 +0100

    Update generate.php

--- generate.php
+++ generate.php
@@ -247,7 +247,7 @@ function brainzTest() {
     //var_dump($brainzstyle); exit();
     
     $arthtml = createArtHTML("DJ Brainz","With MC Whistles","12 Aug 2023", "3-5PM");                      
-    $pdfoutput = createPDFBlob($brainzstyle,$arthtml);
+    $pdfoutput = createPDFBlob($brainzstyle,$arthtml,true);
     $art = pdfToBase64($pdfoutput,"jpg");
     //$art = "performance";
     return $art;
